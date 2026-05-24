1eebd4df8985794ec7658b0c1ec218d62cc15204
FAALopes <faal@example.com>
2026-05-24 17:03:40 +0100

    Switch to CommonJS, use simple Node server, remove ES modules

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run build && node server-http.js
+web: npm run build && npm start
---
+++ server-simple.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+const distDir = path.join(__dirname, 'dist');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(distDir, req.url === '/' ? 'index.html' : req.url);
+
+  fs.stat(filePath, (err, stat) => {
+    if (err || !stat.isFile()) {
+      filePath = path.join(distDir, 'index.html');
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('Not found');
+        return;
+      }
+
+      const mimeType = {
+        '.html': 'text/html',
+        '.js': 'application/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json'
+      }[path.extname(filePath)] || 'application/octet-stream';
+
+      res.writeHead(200, { 'Content-Type': mimeType });
+      res.end(data);
+    });
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
