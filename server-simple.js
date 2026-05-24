674c63bbeceb70b549401f460b2b8cc3d8d79ed4
FAALopes <faal@example.com>
2026-05-24 17:05:25 +0100

    Fix PORT to 8080

--- server-simple.js
+++ server-simple.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 const distDir = path.join(__dirname, 'dist');
 
 const server = http.createServer((req, res) => {
